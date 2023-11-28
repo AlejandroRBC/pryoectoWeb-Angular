88bc5bfadfe3b147429cd72cbf85cfc6b12d1c64
AlejandroRBC <alejandrobernas5@gmail.com>
2023-11-27 23:25:54 -0400

    primeraSubida21/11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioReservaCitaComponent } from './formulario-reserva-cita/formulario-reserva-cita.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioReservaCitaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/formulario-reserva-cita/formulario-reserva-cita.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-reserva-cita',
+  templateUrl: './formulario-reserva-cita.component.html',
+  styleUrls: ['./formulario-reserva-cita.component.css']
+})
+export class FormularioReservaCitaComponent {
+
+}
